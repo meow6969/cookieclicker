1f8e8cc27c5555142ed75aafa81bef30c6a7430d
meow6969 <75815262+meow6969@users.noreply.github.com>
2022-06-06 11:14:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1219,7 +1219,8 @@ var Game={};
 	//replacing an existing canvas picture with a new one at runtime : Game.Loader.Replace('perfectCookie.png','imperfectCookie.png');
 	//upgrades and achievements can use other pictures than icons.png; declare their icon with [posX,posY,'http://example.com/myIcons.png']
 	//check out the "UNLOCKING STUFF" section to see how unlocking achievs and upgrades is done
-	Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js")
+	setTimeout(function(){ Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js") }, 5000);
+	//Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js")
 })();
 
 Game.version=VERSION;
